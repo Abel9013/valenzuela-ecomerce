5db2a30c41e76829b5674060eb814652d55c7a17
Abel <90abel.avr@gmail.com>
2022-07-11 19:36:35 -0300

    add navbar

---
+++ src/components/navBar/Navbar.jsx
@@ -0,0 +1,29 @@
+import "./NavBar.css";
+const NavBar = () => {
+  return (
+    <>
+      <header className="header">
+        <div className="header__contenedor">
+          <div className="header__barra">
+            <div className="navegacion">
+              <a href="##" className="navegacion__link">
+                Inicio
+              </a>
+              <a href="##" className="navegacion__link">
+                Productos
+              </a>
+              <a href="##" className="navegacion__link">
+                Nosotros
+              </a>
+              <a href="##" className="navegacion__link">
+                Contancto
+              </a>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default NavBar;
